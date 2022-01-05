b15a752e79d856f11020da41908385fcdf3bb08b
Huge_alien <zhuhu00@foxmail.com>
2022-01-05 20:45:02 +0800

    initial_commit

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# deeplabv1_ros
-Deeplab segmantic ROS warpper
+# deeplabv1_ros 
+Deeplab segmantic ROS wrapper, ROS中使用DeepLab模型。有关DeepLab的信息在这个[网站](https://github.com/tensorflow/models/tree/master/research/deeplab)可以看到.
+
+推理部分的代码主要参考的是这里：[Colab notebook](https://colab.sandbox.google.com/github/tensorflow/models/blob/master/research/deeplab/deeplab_demo.ipynb).
+
+# How to build
+根据下面的步骤进行安装，需要注意以下部分需要安装`catkin_simple`的包才能够编译成功。
+
+此外，代码中使用的是python2，tensorflow进行推理的，这里需要安装对应python2的版本，可能出现的错误：
+
+![20220105164942](https://raw.githubusercontent.com/zhuhu00/img/master/20220105164942.png)
+解决办法是重新安装protobuf：
+```bash
+pip install protobuf==3.17.3
+```
+
+## Getting started
+Clone this repository to the `src` folder of your catkin workspace, build your workspace and source it.
+
+```bash
+cd <catkin_ws>/src
+git clone https://github.com/zhuhu00/deeplabv1_ros.git
+catkin_make
+source <catkin_ws>/devel/setup.bash
+```
+
+## Example usage
+An example launch file is included processing a sequence from the [Freiburg RGB-D SLAM Dataset](https://vision.in.tum.de/data/datasets/rgbd-dataset/download).
+
+```bash
+cd <catkin_ws>/src/deeplab_ros
+chmod +x scripts/download_freiburg_rgbd_example_bag.sh 
+./scripts/download_freiburg_rgbd_example_bag.sh
+roslaunch deeplab_ros freiburg.launch
+```
+
+## ROS node
+
+#### Parameters:
+
+* **`~rgb_input`** [_string_]
+
+    Topic name of the input RGB stream.
+
+    Default: `"/camera/rgb/image_color"`
+
+* **`~model`** [_string_]
+
+    Name of the backbone network used for inference. List of available models: {"mobilenetv2_coco_voctrainaug", "mobilenetv2_coco_voctrainval", "xception_coco_voctrainaug", "xception_coco_voctrainval"}.
+    If the specified model file doesn't exist, the node automatically downloads the file.
+
+    Default: `"mobilenetv2_coco_voctrainaug"`
+    
+* **`~visualize`** [_bool_]
+
+    If true, the segmentation result overlaid on top of the input RGB image is published to the `~segmentation_viz` topic.
+
+    Default: `true`
+    
+        
+#### Topics subscribed:
+
+* topic name specified by parameter **`~rgb_input`** (default: **`/camera/rgb/image_color`**) [_sensor_mgs/Image_]
+
+    Input RGB image to be processed.
+
+    
+#### Topics published:
+
+* **`~segmentation`** [_sensor_mgs/Image_]
+
+    Segmentation result.
+
+
+* **`~segmentation_viz`** [_sensor_mgs/Image_]
+
+    Visualization-friendly segmentation result color coded with the PASCAL VOC 2012 color map overlaid on top of the input RGB image.
---
+++ src/deeplabv1_ros_node.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+import os
+import tempfile
+import threading
+from six.moves import urllib
+
+import PIL
+import numpy as np
+import cv2
+
+import tensorflow as tf
+
+from cv_bridge import CvBridge
+import rospy
+from sensor_msgs.msg import Image
+
+# from deeplabv1_ros import deeplab
+import deeplab
+
+class DeepLabNode(object):
+    def __init__(self):
+        self._cv_bridge = CvBridge()
+        self._last_msg = None
+        self._msg_lock = threading.Lock()
+
+        self._publish_rate = rospy.get_param('~publish_rate', 100)
+        self._visualize = rospy.get_param('~visualize', True)
+
+        rgb_input = rospy.get_param('~rgb_input', '/camera/rgb/image_color')
+
+        rospy.Subscriber(rgb_input, Image, self._image_callback, queue_size=1)
+
+        self.label_pub = rospy.Publisher('~segmentation', Image, queue_size=1)
+        self.vis_pub = rospy.Publisher('~segmentation_viz', Image, queue_size=1)
+
+        MODEL_NAME = rospy.get_param('~model', 'mobilenetv2_coco_voctrainaug')
+
+        _DOWNLOAD_URL_PREFIX = 'http://download.tensorflow.org/models/'
+        _MODEL_URLS = {
+            'mobilenetv2_coco_voctrainaug':
+                'deeplabv3_mnv2_pascal_train_aug_2018_01_29.tar.gz',
+            'mobilenetv2_coco_voctrainval':
+                'deeplabv3_mnv2_pascal_trainval_2018_01_29.tar.gz',
+            'xception_coco_voctrainaug':
+                'deeplabv3_pascal_train_aug_2018_01_04.tar.gz',
+            'xception_coco_voctrainval':
+                'deeplabv3_pascal_trainval_2018_01_04.tar.gz',
+        }
+
+        # _TARBALL_NAME = 'deeplab_model.tar.gz'
+        _TARBALL_NAME = _MODEL_URLS[MODEL_NAME]
+
+        # download the weights file in temp folder
+        # model_dir = tempfile.mkdtemp()
+        model_dir = rospy.get_param('models_files','../weights')
+        tf.gfile.MakeDirs(model_dir)
+
+        download_path = os.path.join(model_dir, _TARBALL_NAME)
+        print(download_path)
+
+        if(not os.path.exists(download_path)):
+            print('Downloading model, this might take a while...')
+            urllib.request.urlretrieve(_DOWNLOAD_URL_PREFIX + _MODEL_URLS[MODEL_NAME],
+                           download_path)
+            print('Download completed! Loading DeepLab model...')
+
+        print("==Using the model: "+MODEL_NAME )
+        self._model = deeplab.DeepLabModel(download_path)
+        print('Model loaded successfully!')
+
+    def run(self):
+        rate = rospy.Rate(self._publish_rate)
+
+        while not rospy.is_shutdown():
+            if self._msg_lock.acquire(False):
+                msg = self._last_msg
+                self._last_msg = None
+                self._msg_lock.release()
+            else:
+                rate.sleep()
+                continue
+
+            if msg is not None:
+                rgb_image = self._cv_bridge.imgmsg_to_cv2(msg, "passthrough")
+
+                # Run detection.
+                seg_map = self.detect(rgb_image)
+
+                rospy.logdebug("Publishing semantic labels.")
+                # rospy.loginfo("Publishing semantic labels.")
+                label_msg = self._cv_bridge.cv2_to_imgmsg(seg_map, 'mono16')
+                label_msg.header = msg.header
+                self.label_pub.publish(label_msg)
+
+                if self._visualize:
+                    # Overlay segmentation on RGB image.
+                    image = self.visualize(rgb_image, seg_map)
+                    label_color_msg = self._cv_bridge.cv2_to_imgmsg(image, 'bgr8')
+                    label_color_msg.header = msg.header
+                    self.vis_pub.publish(label_color_msg)
+
+
+            rate.sleep()
+
+    def detect(self, rgb_image):
+        rgb_image = PIL.Image.fromarray(rgb_image)
+
+        resized_im, seg_map = self._model.run(rgb_image)
+        seg_map = cv2.resize(seg_map.astype(np.float32), rgb_image.size, interpolation = cv2.INTER_NEAREST).astype(np.uint16)
+
+        return seg_map
+
+    def visualize(self, rgb_image, seg_map, alpha = 0.6):
+        image = rgb_image.copy()
+        seg_image = deeplab.label_to_color_image(seg_map.astype(
+            np.int64)).astype(np.uint8)
+        cv2.addWeighted(seg_image, alpha, image, 1 - alpha, 0, image)
+        return image
+
+
+    def _image_callback(self, msg):
+        rospy.logdebug("Got an image.")
+
+        if self._msg_lock.acquire(False):
+            self._last_msg = msg
+            self._msg_lock.release()
+
+
+def main():
+    rospy.init_node('deeplabv1_ros_node')
+
+    node = DeepLabNode()
+    node.run()
+
+
+if __name__ == '__main__':
+    main()
